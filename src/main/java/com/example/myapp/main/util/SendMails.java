db3c4dc1af4d5596bf921ca4b2cdb90f3937eaa1
Luca <luca.vercelli.to@gmail.com>
2017-06-15 13:55:45 +0200

    pulizia

--- src/main/java/com/example/myapp/main/util/SendMails.java
+++ src/main/java/com/example/myapp/main/util/SendMails.java
@@ -1,11 +1,8 @@
 package com.example.myapp.main.util;
 
-import java.util.Properties;
-
 import javax.annotation.Resource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
