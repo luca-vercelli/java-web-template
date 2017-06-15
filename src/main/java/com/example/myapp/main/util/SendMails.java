835d5f1d454020a7c5acb3b07bbc157b840bc647
Luca <luca.vercelli.to@gmail.com>
2017-06-15 12:41:42 +0200

    mail stuff

---
+++ src/main/java/com/example/myapp/main/util/SendMails.java
@@ -0,0 +1,44 @@
+package com.example.myapp.main.util;
+
+import java.util.Properties;
+
+import javax.annotation.Resource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 
+ * @see https://stackoverflow.com/questions/26548059/sending-email-with-ssl-using-javax-mail
+ * and https://webapps4newbies.blogspot.it/2015/03/configuring-javamail-sessions-in.html
+ *
+ */
+public class SendMails {
+	
+    @Resource(name = "mail/mainMailSession")
+    private Session session;
+    
+	public void simpleSend() {
+
+	    try {
+
+	        Message message = new MimeMessage(session);
+	        message.setFrom(new InternetAddress("frommail@example.com"));
+	        message.setRecipients(Message.RecipientType.TO,
+	                InternetAddress.parse("tomail@example.com"));
+	        message.setSubject("Testing Subject");
+	        message.setText("Test Mail");
+
+	        Transport.send(message);
+
+	        System.out.println("Done");
+
+	    } catch (MessagingException e) {
+	        throw new RuntimeException(e);
+	    }
+	}
+}
