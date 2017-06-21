657ff61ce9e04c9becd1f9283afa7a59e3ddd324
Luca Vercelli <Luca Vercelli@10.102.4.75>
2017-06-21 16:47:46 +0200

    excel v0

--- src/main/java/com/example/myapp/crud/GridManager.java
+++ src/main/java/com/example/myapp/crud/GridManager.java
@@ -7,8 +7,13 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import com.example.myapp.crud.entity.Grid;
 import com.example.myapp.crud.entity.GridColumn;
+import org.apache.poi.ss.usermodel.Cell;
 
 @Stateless
 public class GridManager {
@@ -28,18 +33,59 @@ public class GridManager {
 	 */
 	public List<?> find(Grid grid) {
 
-		String query = "FROM " + grid.getEntity();
-		String comma = ",";
+		String query = "SELECT ";
+		String comma = "";
 		for (GridColumn c : grid.getColumns()) {
+			if (c.getOrder() == null)
+				continue; // not needed
 			query += comma;
 			query += c.getAttributeName();
 			comma = ",";
 		}
-		
-		//TODO order by? filters?
+		query += " FROM " + grid.getEntity();
 
-		return em.createQuery(query).getResultList();
+		// TODO order by? filters?
 
+		List<?> items = em.createQuery(query).getResultList();
+		if (!items.isEmpty()) {
+			System.out.println("CLASS= " + items.get(0).getClass());
+		}
+		return items;
+	}
+
+	/**
+	 * Export grid in XLSX format.
+	 * 
+	 * Similar routines could be implemented for XLS and CSV.
+	 * 
+	 * @param grid
+	 * @return
+	 */
+	public XSSFWorkbook excel(Grid grid) {
+		List<?> items = find(grid);
+
+		XSSFWorkbook wb = new XSSFWorkbook();
+		XSSFSheet sheet = wb.createSheet();
+		Row row = sheet.createRow(0);
+
+		int colnum = 0;
+		for (GridColumn gc : grid.getColumns()) {
+			if (gc.getOrder() == null)
+				continue; // this means not needed in view
+			Cell c = row.createCell(colnum++);
+			// c.setCellStyle(arg0); //TODO
+			c.setCellValue(gc.getDescription()); // TODO i18n
+		}
+
+		for (Object item : items) {
+			colnum = 0;
+			for (GridColumn gc : grid.getColumns()) {
+				Cell c = row.createCell(colnum++);
+				// c.setCellValue(item.method());
+			}
+		}
+
+		return wb;
 	}
 
 }
